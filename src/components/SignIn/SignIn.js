5a03a4367c18a33eeb12a30a908ab27dc055856d
pelumi454 <amoochristianah454@gmail.com>
2022-09-28 12:29:09 +0100

    fix: changes in signin file

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -2,8 +2,50 @@ import React from 'react'
 
 const SignIn = () => {
   return (
-    <div>SignIn</div>
-  )
+    <div className="login">
+      <div className="login-body">
+        <div className="login-header">
+          <img src={logo} alt="calsy-logo" className="logo" />
+          <p className="login-text mt-3">Log into your Calsy account</p>
+        </div>
+        <form>
+          <div>
+            <label for="basic-url" className="form-label">
+              Email Address
+            </label>
+            <div className="input-group mb-3">
+              <Input type="email" name="email" />
+            </div>
+          </div>
+
+          <div>
+            <label for="basic-url" className="form-label">
+              Password
+            </label>
+            <div class="input-group mb-3">
+              <Input
+                type="password"
+                id="basic-url"
+                aria-describedby="basic-addon3"
+                name="password"
+              />
+            </div>
+          </div>
+          <div>
+            <p className="forgot-password">
+              Don't have an account?{" "}
+              <span className="signup-text">Sign up.</span>
+            </p>
+          </div>
+          <div>
+            <button type="submit" className="button">
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default SignIn
